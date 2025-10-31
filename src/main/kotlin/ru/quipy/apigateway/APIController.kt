91a357fd3f06914a666637fe8e4a44021ffe0591
Dmitry Kuznetsov <dmkv2004@mail.ru>
2025-10-31 19:25:35 +0300

    hw5: test 2

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import ru.quipy.common.utils.LeakingBucketRateLimiter
 import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.exceptions.TooManyRequestsException
 import ru.quipy.orders.repository.OrderRepository
@@ -24,9 +25,8 @@ class APIController {
     @Autowired
     private lateinit var orderPayer: OrderPayer
 
-    private val rateLimitPerSec = 11L
-
-    private val rateLimiter = SlidingWindowRateLimiter(rateLimitPerSec, Duration.ofSeconds(1))
+    // 11 * (13 - 1) = 132
+    private val leakingBucketRateLimiter = LeakingBucketRateLimiter(11, Duration.ofSeconds(1), 126)
 
     @PostMapping("/users")
     fun createUser(@RequestBody req: CreateUserRequest): User {
@@ -71,7 +71,7 @@ class APIController {
             it
         } ?: throw IllegalArgumentException("No such order $orderId")
 
-        if (!rateLimiter.tick()) {
+        if (!leakingBucketRateLimiter.tick()) {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build()
         }
 
